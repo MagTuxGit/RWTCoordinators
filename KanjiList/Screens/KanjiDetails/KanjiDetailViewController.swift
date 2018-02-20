5dc1712c5db3175b2e2ecabc09b69d1653cc9e5f
Andriy Trubchanin <magtuxgit@gmail.com>
2018-02-20 16:30:27 +0200

    cleanup

--- KanjiList/Screens/KanjiDetails/KanjiDetailViewController.swift
+++ KanjiList/Screens/KanjiDetails/KanjiDetailViewController.swift
@@ -32,18 +32,6 @@ class KanjiDetailViewController: UIViewController {
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        
-        guard let listViewController = segue.destination as? KanjiListViewController else {
-            
-            return
-        }
-        
-        listViewController.shouldOpenDetailsOnCellSelection = false
-        listViewController.word = sender as? String
-    }
-    
 }
 
 // MARK: - UITableViewDataSource
